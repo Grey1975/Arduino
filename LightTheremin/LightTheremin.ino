078b0de3113688c5adaeae38e35c9b39244ed9b2
Grey1975 <118371728+Grey1975@users.noreply.github.com>
2023-01-03 13:46:59 -0600

    Create LightTheremin.ino
    initial commit

---
+++ LightTheremin/LightTheremin.ino
@@ -0,0 +1,15 @@
+int senseVal;
+int senseLow = 1023;
+int senseHigh = 0;
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
