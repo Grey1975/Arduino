d7b9e406d5b90841b7cd4269d99d6ad172d234b9
Grey1975 <118371728+Grey1975@users.noreply.github.com>
2023-01-03 13:51:45 -0600

    Update LightTheremin.ino
    finished add

--- LightTheremin/LightTheremin.ino
+++ LightTheremin/LightTheremin.ino
@@ -5,11 +5,22 @@ const int ledPin = 13;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  
-
+  digitalWrite(ledPin, HIGH);
+  while (millis() < 5000) {
+    senseVal = analogRead(A0);
+    if (senseVal > senseHigh) {
+      senseHigh = senseVal;
+    }
+    if (senseVal < senseLow) {
+      senseLow = senseVal;
+    }
+  }
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  senseVal = analogRead(A0);
+  int pitch = map(senseVal, senseLow, senseHigh, 50, 4000);
+  tone(8, pitch, 20);
+  delay(10);
 }
